c80ef75eea5ae2b4592b145abd264532d6ff73cc
NguyenKimDien <dienit02@gmail.com>
2017-12-24 12:11:04 +0700

    cập nhật

--- document/restaurant.sql
+++ document/restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 24, 2017 at 06:07 AM
+-- Generation Time: Dec 24, 2017 at 06:10 AM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
