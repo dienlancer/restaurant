95b7058958b8557f3b2266d9d42725cc86b32249
NguyenKimDien <dienit02@gmail.com>
2017-12-24 17:35:44 +0700

    cập nhật

--- wp-content/plugins/com_product/templates/frontend/loop-za-category.php
+++ wp-content/plugins/com_product/templates/frontend/loop-za-category.php
@@ -7,20 +7,10 @@ $productModel=$zController->getModel("/frontend","ProductModel");
 /* begin load config contact */
 $width=$zendvn_sp_settings["product_width"];    
 $height=$zendvn_sp_settings["product_height"];      
-$email_to=$zendvn_sp_settings['email_to'];
-$address=$zendvn_sp_settings['address'];
-$website=$zendvn_sp_settings['website'];
-$telephone=$zendvn_sp_settings['telephone'];
-$contaced_name=$zendvn_sp_settings['contacted_name'];
-$facebook_url=$zendvn_sp_settings['facebook_url'];
-$twitter_url=$zendvn_sp_settings['twitter_url'];
-$google_plus=$zendvn_sp_settings['google_plus'];
-$youtube_url=$zendvn_sp_settings['youtube_url'];
-$instagram_url=$zendvn_sp_settings['instagram_url'];
-$pinterest_url=$zendvn_sp_settings['pinterest_url'];     
+$product_number=$zendvn_sp_settings["product_number"];
 /* end load config contact */
         
-$the_query=$wp_query;
+$query=$wp_query;
 
             // begin phân trang
 $totalItemsPerPage=0;
@@ -32,11 +22,11 @@ if(!empty($zendvn_sp_settings["product_number"])){
 if(!empty(@$_POST["filter_page"]))          {
     $currentPage=@$_POST["filter_page"];  
 }
-$productModel->setWpQuery($the_query);   
+$productModel->setWpQuery($query);   
 $productModel->setPerpage($totalItemsPerPage);        
 $productModel->prepare_items();               
 $totalItems= $productModel->getTotalItems();   
-$the_query=$productModel->getItems();                
+$query=$productModel->getItems();                
 $arrPagination=array(
   "totalItems"=>$totalItems,
   "totalItemsPerPage"=>$totalItemsPerPage,
@@ -46,79 +36,6 @@ $arrPagination=array(
 $pagination=$zController->getPagination("Pagination",$arrPagination);
 ?>
 <form  method="post"  class="frm" name="frm">
-    <input type="hidden" name="filter_page" value="1" /> 
-    <div class="our-member relative">
-        <div class="opacity-absolute"></div>
-        <div class="tarik">
-            <div class="container category-service">
-             <?php single_cat_title(); ?>
-            </div>
-        </div>
-    </div>
-    <div class="container margin-top-15">
-        <div>
-            <?php 
-       if($the_query->have_posts()){  
-        $k=1  ;                             
-        while ($the_query->have_posts()) {
-            $the_query->the_post();     
-            $post_id=$the_query->post->ID;                          
-            $permalink=get_the_permalink($post_id);
-            $title=get_the_title($post_id);
-            $excerpt=get_post_meta($post_id,$meta_key."intro",true);
-            $excerpt=substr($excerpt, 0,100).'...';         
-            $price=get_post_meta($post_id,$meta_key."price",true);
-            $sale_price=get_post_meta($post_id,$meta_key."sale_price",true);
-            $html_price='';                     
-            if((int)($sale_price) > 0){             
-                $price ='<span class="price-regular">'.$vHtml->fnPrice($price).'</span>';
-                $sale_price='<span class="price-sale">'.$vHtml->fnPrice($sale_price).'</span>' ;                    
-                $html_price='<div class="col-xs-6">'.$price.'</div><div class="col-xs-6">'.$sale_price.'</div><div class="clr"></div>' ;                
-            }else{
-                $price='<span class="price-sale">'.$vHtml->fnPrice($price).'</span>' ;  
-                $html_price=$price;     
-            }       
-            $featureImg=wp_get_attachment_url(get_post_thumbnail_id($post_id));                                                     
-            ?>          
-            <div class="col-sm-4 no-padding-left">
-                <div class="perfume-box margin-top-15">
-                    <div><center><a href="<?php echo $permalink; ?>"><figure><img src="<?php echo $featureImg; ?>" /></figure></a></center></div>            
-                    <div class="perfume-title"><a href="<?php echo $permalink; ?>"><?php echo $title; ?></a></div>                      
-                    <div class="perfume-rating">
-                        <center>
-                            <ul >
-                                <li><a href="#"><i class="fa fa-star"></i></a></li>
-                                <li><a href="#"><i class="fa fa-star"></i></a></li>
-                                <li><a href="#"><i class="fa fa-star"></i></a></li>
-                                <li><a href="#"><i class="fa fa-star"></i></a></li>
-                                <li><a href="#"><i class="fa fa-star-o"></i></a></li>
-                            </ul>
-                        </center>
-                    </div>
-                    <div class="perfume-featured-price">
-                        <div><?php echo $html_price; ?></div>
-
-                    </div>
-                    <div class="perfume-cart">                              
-                        <ul>
-                            <li><a href="javascript:void(0)" data-toggle="modal" data-target="#modal-alert-add-cart" onclick="addToCart(<?php echo $post_id; ?>,1);" >Add to cart</a></li>                                      
-                        </ul>
-                    </div>
-                </div>
-            </div>            
-            <?php
-            if($k%3 ==0 || $k==$the_query->post_count){
-                echo '<div class="clr"></div>';
-            }
-            $k++;
-        }               
-        wp_reset_postdata();          
-    }                       
-    ?>
-        </div>       
-        <div>
-            <?php echo $pagination->showPagination();            ?>
-            <div class="clr"></div>
-        </div>
-    </div>    
+    <input type="hidden" name="filter_page" value="1" />     
+    
 </form>
--- wp-content/plugins/com_product/templates/frontend/za-category.php
+++ wp-content/plugins/com_product/templates/frontend/za-category.php
@@ -1,5 +1,68 @@
 <?php get_header();     ?>
-    <?php require_once PLUGIN_PATH . DS . "templates" . DS . "frontend". DS . "loop-za-category.php"; ?>
+    <div class="tina relative">    
+        <div class="oppo">
+            <div class="relative">
+                <div class="nina">
+                    <?php single_cat_title(); ?>
+                </div>
+                <div class="tma"></div>
+            </div>            
+        </div>    
+        <div>
+            <script type="text/javascript" language="javascript">        
+                jQuery(document).ready(function(){
+                    jQuery(".linda").slick({
+                        dots: true,
+                        autoplay:true,
+                        arrows:false,
+                        adaptiveHeight:true,
+                        loop:true
+                    });  
+                });     
+            </script>
+            <div class="linda">
+                <div class="lumberjack">                            
+                    <img src="<?php echo site_url('wp-content/uploads/banner-top.jpg'); ?>" />                               
+                </div>
+            </div>
+        </div>       
+    </div> 
+    <div class="container margin-top-15">
+    	<div class="row">
+    		<div class="col-lg-4">
+    			
+    			<div class="ducati">
+    				<h3>My account</h3>
+    				<div class="margin-top-15">
+    					<?php     
+    					$args = array( 
+    						'menu'              => '', 
+    						'container'         => '', 
+    						'container_class'   => '', 
+    						'container_id'      => '', 
+    						'menu_class'        => 'categoryproductmenu', 
+    						'menu_id'           => 'category-product-menu', 
+    						'echo'              => true, 
+    						'fallback_cb'       => 'wp_page_menu', 
+    						'before'            => '', 
+    						'after'             => '', 
+    						'link_before'       => '', 
+    						'link_after'        => '', 
+    						'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',  
+    						'depth'             => 3, 
+    						'walker'            => '', 
+    						'theme_location'    => 'category-product-menu' 
+    					);
+    					wp_nav_menu($args);
+    					?>   
+    					<div class="clr"></div> 
+    				</div>
+    			</div>
+
+    		</div>
+    		<div class="col-lg-8"></div>
+    	</div>
+    </div>
     <?php get_footer(); ?>
     <?php wp_footer();?>
 </body>
